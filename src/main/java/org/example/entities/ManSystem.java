e22edfd927c7e7047c974828e8e91362c58cbc95
Sean Fang <fangzhengonly@gmail.com>
2023-06-18 14:59:50 -0400

    change users from List to HashMap

--- src/main/java/org/example/entities/ManSystem.java
+++ src/main/java/org/example/entities/ManSystem.java
@@ -1,18 +1,22 @@
 package org.example.entities;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ManSystem {
     public List<Book> books;
     private int currentBookId;
-    public List<User> users;
+//    public List<User> users;
+
+    public HashMap<Integer, User> users ;
     private int currentUserId;
 
     public ManSystem() {
         this.books = new ArrayList<>();
         this.currentBookId = 0;
-        this.users = new ArrayList<>();
+//        this.users = new ArrayList<>();
+        this.users = new HashMap<>();
         this.currentUserId = 0;
     }
 
