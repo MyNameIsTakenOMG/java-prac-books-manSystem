2e9d30198b9ad9fd91293cffb747eb9fd199da64
Sean Fang <fangzhengonly@gmail.com>
2023-06-19 18:23:26 -0400

    implement controlpanel class for the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.entities.Book;
-import org.example.entities.ManSystem;
-import org.example.entities.Menu;
-import org.example.entities.User;
+import org.example.entities.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,102 +10,107 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        var bookSys = new ManSystem();
 
-        String option = "";
-        Scanner userInput = new Scanner(System.in);
+        ControlPanel controlPanel = new ControlPanel(new ManSystem(),new Scanner(System.in));
 
+        controlPanel.start();
 
-        while(!option.equals("q")){
-            System.out.println("call menu... and the option: "+option);
-            Menu.getMenu();
-            option = userInput.nextLine();
-            User theUser;
-            Book theBook;
-            String bookname;
-            int bookId;
-            switch (option) {
-                case "u" -> {
-                    // add a new user
-                    System.out.println("please type username: ");
-                    String username = userInput.nextLine();
-                    bookSys.addUser("John");
-                    // print out the user just created
-                    theUser = bookSys.getUser(bookSys.getCurrentUserId() - 1);
-                    System.out.println("the new user: " + theUser);
-                }
-                case "a" -> {
-                    // add a new book
-                    System.out.println("please type bookname: ");
-                    bookname = userInput.nextLine();
-                    bookSys.addBook(bookname);
-                    theBook = bookSys.getBookById(bookSys.getCurrentBookId() - 1);
-                    System.out.println("the new book: " + theBook);
-                }
-                case "i" -> {
-                    // get a book by id
-                    System.out.println("please type book id");
-//                    bookId = userInput.nextInt();
-                    bookId = Integer.parseInt(userInput.nextLine());
-                    theBook = bookSys.books.get(bookId);
-                    System.out.println("the searched book: " + theBook);
-                }
-                case "n" -> {
-                    // get books by name
-                    System.out.println("please type book name");
-                    bookname = userInput.nextLine();
-                    var searchingBooks = bookSys.getBooksByName(bookname);
-                    System.out.println("searching book result: " + searchingBooks);
-                }
-                case "b", "r" -> {
-                    // borrow books
-                    borrowOrReturn(option,bookSys,userInput);
-                }
-                // return books
-                default -> {
-                }
-            }
-        }
-        System.out.println("management system is closed");
+//        var bookSys = new ManSystem();
+//
+//        String option = "";
+//        Scanner userInput = new Scanner(System.in);
+//
+//
+//        while(!option.equals("q")){
+//            System.out.println("call menu... and the option: "+option);
+//            Menu.getMenu();
+//            option = userInput.nextLine();
+//            User theUser;
+//            Book theBook;
+//            String bookname;
+//            int bookId;
+//            switch (option) {
+//                case "u" -> {
+//                    // add a new user
+//                    System.out.println("please type username: ");
+//                    String username = userInput.nextLine();
+//                    bookSys.addUser(username);
+//                    // print out the user just created
+//                    theUser = bookSys.getUser(bookSys.getCurrentUserId() - 1);
+//                    System.out.println("the new user: " + theUser);
+//                }
+//                case "a" -> {
+//                    // add a new book
+//                    System.out.println("please type bookname: ");
+//                    bookname = userInput.nextLine();
+//                    bookSys.addBook(bookname);
+//                    theBook = bookSys.getBookById(bookSys.getCurrentBookId() - 1);
+//                    System.out.println("the new book: " + theBook);
+//                }
+//                case "i" -> {
+//                    // get a book by id
+//                    System.out.println("please type book id");
+////                    bookId = userInput.nextInt();
+//                    bookId = Integer.parseInt(userInput.nextLine());
+//                    theBook = bookSys.books.get(bookId);
+//                    System.out.println("the searched book: " + theBook);
+//                }
+//                case "n" -> {
+//                    // get books by name
+//                    System.out.println("please type book name");
+//                    bookname = userInput.nextLine();
+//                    var searchingBooks = bookSys.getBooksByName(bookname);
+//                    System.out.println("searching book result: " + searchingBooks);
+//                }
+//                case "b", "r" -> {
+//                    // borrow books
+//                    borrowOrReturn(option,bookSys,userInput);
+//                }
+//                // return books
+//                default -> {
+//                }
+//            }
+//        }
+//        System.out.println("management system is closed");
 
     }
-    public static void borrowOrReturn(String option, ManSystem bookSys, Scanner userInput){
-        String operation = option.equals("b")? "borrow":"return";
-        // borrow or return books
-        System.out.println("please type user id");
-//                    userId = userInput.nextInt();
-        int userId = Integer.parseInt(userInput.nextLine());
-        User theUser = bookSys.users.get(userId);
-        // take book id(s)
-        List<Integer> bookIds = generateList(userInput);
-        if(operation.equals("borrow"))
-            bookSys.borrowBooks(
-                    userId,
-                    bookIds.get(0),
-                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
-            );
-        else
-            bookSys.returnBooks(
-                    userId,
-                    bookIds.get(0),
-                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
-            );
-        System.out.println("borrowed books: " + theUser.getBorrowedBooks());
-        System.out.println("first book borrowed by: " + bookSys.books.get(bookIds.get(0)).getRentedBy());
-    }
+//    public static void borrowOrReturn(String option, ManSystem bookSys, Scanner userInput){
+//        String operation = option.equals("b")? "borrow":"return";
+//        // borrow or return books
+//        System.out.println("please type user id");
+////                    userId = userInput.nextInt();
+//        int userId = Integer.parseInt(userInput.nextLine());
+//        User theUser = bookSys.users.get(userId);
+//        // take book id(s)
+//        List<Integer> bookIds = generateList(userInput);
+//        if(operation.equals("borrow"))
+//            bookSys.borrowBooks(
+//                    userId,
+//                    bookIds.get(0),
+//                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
+//            );
+//        else
+//            bookSys.returnBooks(
+//                    userId,
+//                    bookIds.get(0),
+//                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
+//            );
+//        System.out.println("borrowed books: " + theUser.getBorrowedBooks());
+//        System.out.println("first book borrowed by: " + bookSys.books.get(bookIds.get(0)).getRentedBy());
+//    }
 
-    public static List<Integer> generateList(Scanner input){
-        String continuing = "y";
-        List<Integer> theList = new ArrayList<>();
-        int num;
-        while (continuing.equals("y")) {
-            System.out.println("please enter a number");
-//                        bookId = userInput.nextInt();
-            num = Integer.parseInt(input.nextLine());
-            theList.add(num);
-            System.out.println("continue to? (y/n)");
-            continuing = input.nextLine();
-        }
-        return theList;
-    }
+//    public static List<Integer> generateList(Scanner input){
+//        String continuing = "y";
+//        List<Integer> theList = new ArrayList<>();
+//        int num;
+//        while (continuing.equals("y")) {
+//            System.out.println("please enter a number");
+////                        bookId = userInput.nextInt();
+//            num = Integer.parseInt(input.nextLine());
+//            theList.add(num);
+//            System.out.println("continue to? (y/n)");
+//            continuing = input.nextLine();
+//        }
+//        return theList;
+//    }
 }
---
+++ src/main/java/org/example/entities/ControlPanel.java
@@ -0,0 +1,114 @@
+package org.example.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ControlPanel {
+    private ManSystem bookSys;
+    private String option;
+    private Scanner userInput;
+
+    public ControlPanel(ManSystem bookSys, Scanner userInput) {
+        this.bookSys = bookSys;
+        this.option = "";
+        this.userInput = userInput;
+    }
+
+    public void start(){
+        while(!option.equals("q")){
+//            System.out.println("call menu... and the option: "+option);
+            Menu.getMenu();
+            option = userInput.nextLine();
+            switch (option) {
+                case "u" -> {
+                    addUser();
+                }
+                case "a" -> {
+                    addBook();
+                }
+                case "i" -> {
+                    getBookById();
+                }
+                case "n" -> {
+                    getBooksByName();
+                }
+                case "b", "r" -> {
+                    borrowOrReturn();
+                }
+                default -> {
+                }
+            }
+        }
+        System.out.println("management system is closed");
+    }
+    public void getBooksByName(){
+        System.out.println("please type book name");
+        String bookname = userInput.nextLine();
+        var searchingBooks = bookSys.getBooksByName(bookname);
+        System.out.println("searching book result: " + searchingBooks);
+    }
+    public void getBookById(){
+        System.out.println("please type book id");
+//                    bookId = userInput.nextInt();
+        int bookId = Integer.parseInt(userInput.nextLine());
+        Book theBook = bookSys.books.get(bookId);
+        System.out.println("the searched book: " + theBook);
+    }
+
+    public void addBook(){
+        System.out.println("please type bookname: ");
+        String bookname = userInput.nextLine();
+        bookSys.addBook(bookname);
+        Book theBook = bookSys.getBookById(bookSys.getCurrentBookId() - 1);
+        System.out.println("the new book: " + theBook);
+    }
+    public void addUser(){
+        System.out.println("please type username: ");
+        String username = userInput.nextLine();
+        bookSys.addUser(username);
+        // print out the user just created
+        User theUser = bookSys.getUser(bookSys.getCurrentUserId() - 1);
+        System.out.println("the new user: " + theUser);
+    }
+
+    public void borrowOrReturn(){
+        String operation = option.equals("b")? "borrow":"return";
+        // borrow or return books
+        System.out.println("please type user id");
+//                    userId = userInput.nextInt();
+        int userId = Integer.parseInt(userInput.nextLine());
+        User theUser = bookSys.users.get(userId);
+        // take book id(s)
+        List<Integer> bookIds = generateList();
+        if(operation.equals("borrow"))
+            bookSys.borrowBooks(
+                    userId,
+                    bookIds.get(0),
+                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
+            );
+        else
+            bookSys.returnBooks(
+                    userId,
+                    bookIds.get(0),
+                    bookIds.subList(1, bookIds.size()).toArray(new Integer[0])
+            );
+        System.out.println("borrowed books: " + theUser.getBorrowedBooks());
+        System.out.println("first book borrowed by: " + bookSys.books.get(bookIds.get(0)).getRentedBy());
+    }
+
+    public List<Integer> generateList(){
+        String continuing = "y";
+        List<Integer> theList = new ArrayList<>();
+        int num;
+        while (continuing.equals("y")) {
+            System.out.println("please enter a number");
+//                        bookId = userInput.nextInt();
+            num = Integer.parseInt(userInput.nextLine());
+            theList.add(num);
+            System.out.println("continue? (y/n)");
+            continuing = userInput.nextLine();
+        }
+        return theList;
+    }
+}
